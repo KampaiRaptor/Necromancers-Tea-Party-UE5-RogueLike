0967417eecec7e70b3b008f6005bc4ff74903f93
Fansi129 <120315901+Fansi129@users.noreply.github.com>
2023-07-27 23:37:43 +0200

    Project Origin

---
+++ Source/StateTree/StateTree.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StateTree.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StateTree, "StateTree" );
---
+++ Source/StateTree/StateTreeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StateTreeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STATETREE_API AStateTreeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
